4ff7a7effdfcce3d773409a7fb707d5cfc543628
huangxuesong <626732786@qq.com>
2020-11-19 18:19:16 +0800

    新增:渲染实际盘符,可进入对应盘符,可递归点击文件夹

--- renderJS/index/index.js
+++ renderJS/index/index.js
@@ -1,8 +1,13 @@
-const { BrowserWindow } = require('electron').remote
-const fs = require('fs');
+const {
+  BrowserWindow
+} = require('electron').remote
+const fs = require('fs-extra');
 const os = require('os');
 const path = require('path');
-const exec = require('child_process').exec;
+const d = require('diskinfo');
+const {
+  dir
+} = require('console');
 
 let main = {
   dom: {
@@ -11,25 +16,64 @@ let main = {
     MAX_BTN: $('.max-btn'),
     CLOSE_BTN: $('.close-btn'),
     CATALOGUE_INPUT: $('.catalogue'),
-    SEARCH_INPUT: $('.search')
+    SEARCH_INPUT: $('.search'),
+    SHOW_CONTENT: $('.show-content'),
+    DEV_TOTAL: $('.dev-total'),
+    DISK: $('.disk')
   },
-  temp:{
-    document(){
-
+  data: {
+    basePath: '',
+  },
+  temp: {
+    directory(param) {
+      const {
+        name,
+        updateDate,
+        type,
+        size
+      } = param
+      return `
+        <tr class="item" data-path="${name ? name : ''}/">
+          <td><img class="name" src="./assets/img/directory.png" alt="">${name ? name : '未知名'}</td>
+          <td class="update-date">${updateDate ? updateDate : ''}</td>
+          <td class="type">${type ? type : ''}</td>
+          <td class="size">${size ? size : ''}</td>
+        </tr>
+      `
     },
-    disk(){
-
+    disk(param) {
+      const {
+        diskName,
+        usedRate,
+        canUse,
+        size
+      } = param
+      return `
+      <li class="box" data-path="${diskName ? diskName : ''}/">
+        <img src="./assets/img/disk.png" alt="">
+        <div class="disk-info">
+          <span class="disk-name">${diskName ? diskName : '未知盘名'}</span>
+          <div class="disk-ram-bar">
+            <div class="bar-content" style="width:${usedRate ? usedRate : 100}"></div>
+          </div>
+          <span class="disk-ram-text">${canUse ? canUse : 'xx'}GB可用，共${size ? size : 'xx'}GB</span>
+        </div>
+      </li>
+      `
     }
   },
   init() {
     this.eventListener();
+    this.getDiskInfo();
   },
   eventListener() {
     this.windowEvent();
+    this.getInItemEvent();
+    this.getInDirEvent();
   },
+  //窗口按钮事件监听
   windowEvent() {
     this.dom.HEADER.on('click', 'li', function (evt) {
-      console.log('ev', $(this).attr('class'))
       let className = $(this).attr('class'),
         win = BrowserWindow.getFocusedWindow()
       switch (className) {
@@ -60,7 +104,87 @@ let main = {
       $(`.${className}`).css('filter', '')
     })
   },
-  
+  //获取盘符信息
+  getDiskInfo() {
+    d.getDrives((err, aDrives) => {
+      if (err) throw err;
+      let diskListHtml = '';
+      for (var i = 0; i < aDrives.length; i++) {
+        // console.log('描述' + aDrives[i].filesystem);
+        // console.log('总空间 ' + aDrives[i].blocks);
+        // console.log('已使用' + aDrives[i].used);
+        // console.log('可用' + aDrives[i].available);
+        // console.log('已用百分比' + aDrives[i].capacity);
+        // console.log('盘符' + aDrives[i].mounted);
+        // console.log('-----------------------------------------');
+        let obj = {
+          diskName: aDrives[i].mounted,
+          usedRate: aDrives[i].capacity,
+          canUse: (aDrives[i].available / 1024 / 1024 / 1024).toFixed(2).slice(0, -1),
+          size: (aDrives[i].blocks / 1024 / 1024 / 1024).toFixed(2).slice(0, -1)
+        }
+        diskListHtml += this.temp.disk(obj)
+      }
+      $(' DEV_TOTAL')
+      this.dom.DEV_TOTAL.text(aDrives.length)
+      this.dom.DISK.html(diskListHtml);
+    })
+  },
+  getInItemEvent() {
+    this.dom.SHOW_CONTENT.on('dblclick', 'li', (evt) => {
+      console.log('evt', evt.currentTarget.dataset.path)
+      const {
+        path
+      } = evt.currentTarget.dataset;
+      this.data.basePath += path;
+      this.showCurrentDir(this.data.basePath);
+    })
+  },
+  getInDirEvent() {
+    this.dom.SHOW_CONTENT.on('dblclick', 'tr', (evt) => {
+      console.log('evt', evt.currentTarget.dataset.path)
+      const {
+        path
+      } = evt.currentTarget.dataset;
+      this.data.basePath += path;
+      this.showCurrentDir(this.data.basePath);
+    })
+  },
+  showCurrentDir(path) {
+    fs.readdir(path, (err, files) => {
+      if (err) throw err;
+      console.log('showCurrentDir', files)
+      this.renderCurrentDir(files)
+    })
+  },
+  renderCurrentDir(dirArr) {
+    let dirListHtml = `
+      <table class="table">
+        <thead class="thead">
+          <tr>
+            <th class="th">名称</th>
+            <th class="th">修改日期</th>
+            <th class="th">类型</th>
+            <th class="th">大小</th>
+          </tr>
+        </thead>
+        <tbody class="tbody">
+    `;
+
+    for (let i = 0, l = dirArr.length; i < l; i++) {
+      let obj = {
+        name: dirArr[i]
+      }
+      dirListHtml += this.temp.directory(obj)
+    }
+    dirListHtml += `
+        </tbody>
+      </table>
+    `
+    console.log('asx', dirListHtml)
+    this.dom.SHOW_CONTENT.html(dirListHtml)
+
+  }
 }
 
 
@@ -71,36 +195,13 @@ $(function () {
 try {
   let homedir = os.homedir()
   console.log(homedir)
-  // getDir(homedir)
-  console.log('x',showDisk());
-  $('.disk').text(showDisk());
 
-  // let data = fs.readFileSync('C:\\Users\\roy\\Desktop\\1.txt', 'utf8');
-  let data = fs.readFileSync('C:\\Users\\roy\\Desktop\\1.txt', 'utf8');
-  console.log(data)
+  fs.readdir('D:/', (err, files) => {
+    if (err) throw err;
+    console.log('files', files)
+  })
+
 
 } catch (e) {
   console.log('读取文件发生错误')
-}
-
-async function getDir(path) {
-  const dir = await fs.promises.opendir(path);
-  for await (const dirent of dir) {
-    console.log(dirent.name);
-  }
-}
-
-function showDisk(){
-  exec('wmic logicaldisk get name,Description,filesystem,size,freespace  /format:list', (err, stdout, stderr)=>{
-    if(err){
-      console.error(`exec error: ${err}`)
-      return;
-    }
-    console.log('r',stdout)
-    $('.disk').text(stdout);
-    return stdout
-    let aLines = stdout.split('\r\r\n');
-    console.log(`stdout: ${aLines}`);
-    console.error(`stderr: ${stderr}`);
-  })
 }
